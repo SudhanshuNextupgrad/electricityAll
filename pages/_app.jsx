653057c5e48e2cb496c1d960333e571459de7ab3
SudhanshuNextupgrad <sudhanshu.nextupgrad@gmail.com>
2024-01-24 00:29:27 +0530

    global.css added

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,8 +1,5 @@
 import '@/styles/globals.css'
-import "@/styles/employee.css"
-import "@/styles/admin.css"
-import "@/styles/globals2.css"
-import "@/styles/globals3.css"
+
 
 
 export default function App({ Component, pageProps }) {
